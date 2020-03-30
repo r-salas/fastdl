164081554a66ea8267553f241a3efb096c25d7b3
Ruben Salas <ruben.salasalcornada@telefonica.com>
2020-03-30 13:20:28 +0200

    Update downloader to add optional headers and force options

--- fastdl/downloader.py
+++ fastdl/downloader.py
@@ -8,21 +8,20 @@ import os
 import warnings
 
 from .utils import splitext
-from .parallel import parallel
 from .hasher import validate_file
 from .extractor import extract_file, can_extract
 
 from tqdm import tqdm
 from urllib.parse import urlparse
-from urllib.request import urlopen
+from urllib.request import urlopen, Request
 from urllib.error import ContentTooShortError
 
 
-@parallel
-def download(url, fname=None, dir_prefix=".", blocksize=1024 * 8, file_hash=None, hash_algorithm="auto",
-             extract=False, extract_dir=None, progressbar=True):
-    file_path = urlretrieve(url, fname=fname, dir_prefix=dir_prefix, blocksize=blocksize,
-                            progressbar=progressbar, file_hash=file_hash, hash_algorithm=hash_algorithm)
+def download(url, fname=None, dir_prefix=".", headers=None, blocksize=1024 * 8, file_hash=None, hash_algorithm="auto",
+             extract=False, extract_dir=None, progressbar=True, force_download=False, force_extraction=False):
+    file_path = _urlretrieve(url, fname=fname, dir_prefix=dir_prefix, headers=headers, blocksize=blocksize,
+                             progressbar=progressbar, file_hash=file_hash, hash_algorithm=hash_algorithm,
+                             force_download=False)
 
     if not extract:
         return file_path
@@ -30,21 +29,25 @@ def download(url, fname=None, dir_prefix=".", blocksize=1024 * 8, file_hash=None
     if extract_dir is None:
         extract_dir, _ = splitext(file_path)
 
-    if can_extract(file_path):
-        return extract_file(file_path, extract_dir, progressbar=progressbar)
-    else:
+    if not can_extract(file_path):
         warnings.warn("`extract=True` but {} can't be extracted".format(file_path))
         return file_path
 
+    return extract_file(file_path, extract_dir, force=force_extraction, progressbar=progressbar)
 
-@parallel
-def urlretrieve(url, fname=None, dir_prefix=".", blocksize=1024 * 8, progressbar=True, reporthook=None, file_hash=None,
-                hash_algorithm="auto"):
+
+def _urlretrieve(url, fname=None, dir_prefix=".", headers=None, blocksize=1024 * 8, progressbar=True, reporthook=None,
+                 file_hash=None, hash_algorithm="auto", force_download=False):
     """
-    A more advance version of urllib.request.urlretrieve with support of progress bars, automatic file name,
-    cache and file hash
+    A more advance version of urllib.request.urlretrieve with support of progress bars,
+    automatic file name, cache and file hash
     """
-    with urlopen(url) as response:
+    if headers is None:
+        headers = {}
+
+    request = Request(url, headers=headers)
+
+    with urlopen(request) as response:
         headers = response.info()
 
         if fname is None:
@@ -59,7 +62,7 @@ def urlretrieve(url, fname=None, dir_prefix=".", blocksize=1024 * 8, progressbar
             os.makedirs(dir_prefix, exist_ok=True)
             file_path = os.path.join(dir_prefix, fname)
 
-        if os.path.exists(file_path):
+        if os.path.exists(file_path) and not force_download:
             if file_hash is not None and not validate_file(file_path, file_hash, hash_algorithm):
                 warnings.warn("A local file was found, but it seems to be incomplete or outdated because the " +
                               hash_algorithm + " file hash does not match the original value of " + file_hash +
