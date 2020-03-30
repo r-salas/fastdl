ff8e3a28d0db9b0c8aa94258659a4ec98255724a
Ruben Salas <ruben.salasalcornada@telefonica.com>
2020-03-30 13:21:16 +0200

    Update public API

--- fastdl/__init__.py
+++ fastdl/__init__.py
@@ -1,6 +1,6 @@
-from . import utils  # noqa: E501
+from . import utils  # noqa: F401
 
-from .extractor import extract_file  # noqa: E501
-from .downloader import download, urlretrieve  # noqa: E501
-from .hasher import hash_file  # noqa: E501
-from .parallel import parallel, get, Blueprint  # noqa: E501
+from .extractor import extract_file  # noqa: F401
+from .downloader import download  # noqa: F401
+from .hasher import hash_file, validate_file  # noqa: F401
+from .parallel import Parallel  # noqa: F401
