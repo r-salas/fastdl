fa6abb4c578816a96cc67a9942015ab14be6bb39
Ruben Salas <ruben.salasalcornada@telefonica.com>
2020-03-30 13:19:35 +0200

    Don't re-extract if already exists

--- fastdl/extractor.py
+++ fastdl/extractor.py
@@ -8,13 +8,17 @@ import zipfile
 import tarfile
 
 from tqdm import tqdm
+from os.path import isfile, join
 
 
 def can_extract(file_path):
+    """
+    Whether or not fastdl can extract this file.
+    """
     return _to_extractor(file_path) is not None
 
 
-def extract_file(file_path, extract_dir=".", progressbar=True, ):
+def extract_file(file_path, extract_dir=".", progressbar=True, force=False):
     """
     Unpack `file_path` to `extract_dir`.
     """
@@ -23,10 +27,10 @@ def extract_file(file_path, extract_dir=".", progressbar=True, ):
     if extractor is None:
         raise ValueError("File {} not supported for extraction".format(file_path))
 
-    return extractor(file_path, extract_dir, progressbar)
+    return extractor(file_path, extract_dir, progressbar=progressbar, force=force)
 
 
-def _extract_zip(file_path, extract_dir=".", progressbar=True):
+def _extract_zip(file_path, extract_dir=".", progressbar=True, force=False):
     """
     Unpack zip `filename` to `extract_dir`
     """
@@ -36,13 +40,14 @@ def _extract_zip(file_path, extract_dir=".", progressbar=True):
         if progressbar:
             iterator = tqdm(iterator, total=len(iterator), unit="", desc="Extracting files...")
 
-        for file in iterator:
-            zip_file.extract(member=file, path=extract_dir)
+        for member in iterator:
+            if not isfile(join(extract_dir, member)) or force:
+                zip_file.extract(member=member, path=extract_dir)
 
     return extract_dir
 
 
-def _extract_tar(file_path, extract_dir=".", progressbar=True):
+def _extract_tar(file_path, extract_dir=".", progressbar=True, force=False):
     """
     Unpack tar/tar.gz/tar.bz2
     """
@@ -53,7 +58,8 @@ def _extract_tar(file_path, extract_dir=".", progressbar=True):
             iterator = tqdm(iterator, total=len(iterator), unit="", desc="Extracting files...")
 
         for member in iterator:
-            tarball.extract(member=member, path=extract_dir)
+            if not isfile(join(extract_dir, member)) or force:
+                tarball.extract(member=member, path=extract_dir)
 
     return extract_dir
 
@@ -63,5 +69,3 @@ def _to_extractor(file_path):
         return _extract_zip
     elif tarfile.is_tarfile(file_path):
         return _extract_tar
-
-    return None
