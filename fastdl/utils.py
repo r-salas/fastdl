f512c9f2b6c435fe22491fc428f8f1aea6816aaa
Ruben Salas <ruben.salasalcornada@telefonica.com>
2020-03-30 13:21:31 +0200

    Add lines function to utils

--- fastdl/utils.py
+++ fastdl/utils.py
@@ -27,3 +27,26 @@ def splitext(path):
         if path.endswith(ext):
             return path[:-len(ext)], path[-len(ext):]
     return os.path.splitext(path)
+
+
+def lines(file_path, strip=True):
+    """
+    Read file line by line
+
+    Parameters
+    -----------
+    file_path: str
+        File path
+    strip: bool
+        Whether or not strip spaces from each line
+
+    Yields
+    ---------
+    line: str
+        Read line.
+    """
+    with open(file_path) as f:
+        for line in f:
+            if strip:
+                line = line.strip()
+            yield line
