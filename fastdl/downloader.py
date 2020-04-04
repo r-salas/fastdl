419ac9a6d3731813cc5d2ba41f78a4630a1ea208
Ruben Salas <ruben.salasalcornada@telefonica.com>
2020-04-04 19:27:10 +0200

    Download to temporary file before download is finished

--- fastdl/downloader.py
+++ fastdl/downloader.py
@@ -132,9 +132,11 @@ def _urlretrieve(url, fname=None, dir_prefix=".", headers=None, blocksize=1024 *
         blocknum = 0
         bytes_read = 0
 
-        with open(file_path, "wb") as fp, tqdm(total=content_length, unit='B', unit_scale=True, miniters=1,
-                                               unit_divisor=1024, desc="Downloading {}...".format(fname),
-                                               disable=not progressbar) as pbar:
+        download_file_path = file_path + ".download"
+
+        with open(download_file_path, "wb") as fp, tqdm(total=content_length, unit='B', unit_scale=True, miniters=1,
+                                                        unit_divisor=1024, desc="Downloading {}...".format(fname),
+                                                        disable=not progressbar) as pbar:
             while True:
                 block = response.read(blocksize)
                 if not block:
@@ -153,6 +155,8 @@ def _urlretrieve(url, fname=None, dir_prefix=".", headers=None, blocksize=1024 *
 
     if content_length >= 0 and bytes_read < content_length:
         error_msg = "retrieval incomplete: got only {} out of {} bytes".format(bytes_read, content_length)
-        raise ContentTooShortError(error_msg, (file_path, headers))
+        raise ContentTooShortError(error_msg, (download_file_path, headers))
+
+    os.rename(download_file_path, file_path)
 
     return file_path
